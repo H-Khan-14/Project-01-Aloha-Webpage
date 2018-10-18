dc0edf769eebc4c9d8e7b879d98e043e06ac0320
H-Khan-14 <haseebkhan14@hotmail.com>
2018-10-18 10:08:28 -0400

    Updated media query and smooth scroll

--- Aloha-P1.js
+++ Aloha-P1.js
@@ -13,4 +13,19 @@ $(document).ready(function() {
     alert('Item Added to Cart');
     $('.red-integer').text(count++);
   });
+
+  $('.about-li').on('click', function() {
+    document
+      .querySelector('.bbanner-text')
+      .scrollIntoView({ behavior: 'smooth' });
+  });
+  $('.shop-li').on('click', function() {
+    document.querySelector('#featured').scrollIntoView({ behavior: 'smooth' });
+  });
+  $('.featured-li').on('click', function() {
+    document.querySelector('#shop').scrollIntoView({ behavior: 'smooth' });
+  });
+  $('.updates-li').on('click', function() {
+    document.querySelector('#update').scrollIntoView({ behavior: 'smooth' });
+  });
 });
